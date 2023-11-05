f730d774df4cc9b86bed0a9617a4948d7dd2b69c
Nikhilan <125346233+itsnicky35@users.noreply.github.com>
2023-11-05 20:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 3,
+        slideShadows: true
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 1
+        },
+        1024: {
+            slidesPerView: 2
+        },
+        1560: {
+            slidesPerView: 3
+        }
+    }
+});
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
